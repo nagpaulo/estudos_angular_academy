219aa2b08aa1be46f1ba9f2b0097017801b81a7f
Paulo Roberto <paulo.roberto@ASTIN-1877405.seduc.ce.gov.br>
2019-11-27 16:59:10 -0300

    Header Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
 import { XyzComponent } from './xyz/xyz.component';
 import { from } from 'rxjs';
@@ -11,7 +12,8 @@ import { from } from 'rxjs';
 @NgModule({
   declarations: [
     AppComponent,
-    XyzComponent
+    XyzComponent,
+    HeaderComponent
   ],
   imports: [
     HttpClientModule,
